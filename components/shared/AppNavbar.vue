4d71e5abc443548da76f74b8961ee2a69a488ffc
alialaca <alialaca0@gmail.com>
2025-03-20 14:04:28 +0300

    fix: adjust button styling in AppNavbar for improved accessibility and consistency

--- components/shared/AppNavbar.vue
+++ components/shared/AppNavbar.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template lang="pug">
-  nav(class="flex items-center justify-between p-8 lg:px-8" aria-label="Global")
+  nav(class="flex items-center justify-between py-8 " aria-label="Global")
     div(class="flex lg:flex-1")
       a(href="#" class="-m-1.5 p-1.5")
         span(class="sr-only") FixPro
         nuxt-img(src="/logo.png" class="h-8")
     div(class="flex lg:hidden")
-      button(type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700")
+      button(type="button" class="-m-2.5 inline-flex items-center justify-center rounded-sm p-2.5 text-gray-700")
         span(class="sr-only") Open main menu
         svg(class="size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon")
           path(stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5")
