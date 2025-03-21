2819dbb2f276c24b5c5b85b99d918471f2895a03
alialaca <alialaca0@gmail.com>
2025-03-21 13:45:40 +0300

    feat: add kayit.vue component for registration page layout

---
+++ pages/kayit.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="pug">
+main.w-screen.h-screen.bg-fwhite
+  div(class="w-4/12 h-screen bg-fxsoftblue grid place-content-center")
+    nuxt-img(src="/images/register.png" alt="register")
+  div(class="w-4/12 h-screen bg-fxsoftblue grid place-content-center")
+    nuxt-img(src="/images/register.png" alt="register")
+</template>
+
+<style scoped>
+
+</style>
