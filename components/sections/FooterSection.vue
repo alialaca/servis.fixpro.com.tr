b61fa6c26b42d1d25fab40c356e4a628b0a02c86
alialaca <alialaca0@gmail.com>
2025-04-07 17:10:02 +0300

    feat: update FooterSection component with social media links and contact information

--- components/sections/FooterSection.vue
+++ components/sections/FooterSection.vue
@@ -1,18 +1,42 @@
 <script setup lang="ts">
 
+import Logo from "~/components/shared/Logo.vue";
+import Divider from "~/components/shared/Divider.vue";
+const accounts = [
+  { icon: 'mdi:instagram', link: 'https://www.instagram.com/fixpro_tr/'},
+  { icon: 'mdi:facebook', link: 'https://www.instagram.com/fixpro_tr/'},
+  { icon: 'hugeicons:tiktok', link: 'https://www.instagram.com/fixpro_tr/'},
+  { icon: 'mdi:youtube', link: 'https://www.instagram.com/fixpro_tr/'},
+  { icon: 'mdi:linkedin', link: 'https://www.instagram.com/fixpro_tr/'},
+  { icon: 'hugeicons:new-twitter', link: 'https://www.instagram.com/fixpro_tr/'}
+]
+
+const contacts = [
+  { icon: 'ic:outline-location-on', text: 'Yedpa Ticaret Merkezi / İstanbul', link: '' },
+  { icon: 'ic:outline-phone-in-talk', text: '+90 232 700 24 84', link: '' },
+  { icon: 'ic:sharp-language', text: 'www.fixpro.com.tr', link: 'https://www.fixpro.com.tr' },
+  { icon: 'ic:baseline-access-time', text: 'Pazartesi - Cuma / 09.00 - 17.00', link: '' }
+]
 </script>
 
 <template lang="pug">
 section#iletisim(class="w-screen bg-fxsoftblue")
-  div.container.mx-auto.py-32
-    footer(class="flex items-center justify-between")
-      nuxt-img(src="/logo.png" class="w-40")
-      div.contacts.text-fxblue.flex.gap-8
-        Icon(name="ic:outline-location-on" size="1.5em")
-        Icon(name="ic:outline-phone-in-talk" size="1.5em")
-        Icon(name="ic:sharp-language" size="1.5em")
-        Icon(name="ic:baseline-access-time" size="1.5em")
-  div.text-center.font-light.p-4 Copyright © 2025 | FixPro İç ve Dış Ticaret A.Ş
+  div.container.mx-auto.pt-16.pb-8
+    footer(class="flex items-start justify-between")
+      div.flex.flex-col.gap-3.text-fxblue
+        div.flex.items-end.gap-3
+          Logo
+          div(class="bg-fxblue h-[2rem] w-[1px]")
+          p.font-light Teknik Servis
+        div(class="bg-fxblue w-full h-[1px]")
+        div.flex.justify-between.text-fxblue
+          LazyIcon(v-for="acount in accounts" :key="acount.link" :name="acount.icon")
+        div.text-xs.mt-10 Copyright © 2025 | FixPro İç ve Dış Ticaret A.Ş
+
+      div.contacts.flex.flex-col.gap-2
+        a.flex.items-center(v-for="contact in contacts" :key="contact.text")
+          Icon(:name="contact.icon" size="1em")
+          p.ml-2 {{contact.text}}
 
 </template>
 
