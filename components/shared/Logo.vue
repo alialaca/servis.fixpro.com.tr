471cb9b9617e54d606251fbb9da64d0fe22b6c05
alialaca <alialaca0@gmail.com>
2025-04-07 17:11:15 +0300

    feat: add Logo component with image and accessibility features

---
+++ components/shared/Logo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template lang="pug">
+a(href="#" class="-m-1.5 p-1.5")
+  span(class="sr-only") FixPro
+  nuxt-img(src="/logo.png" class="h-8")
+</template>
+
+<style scoped>
+
+</style>
