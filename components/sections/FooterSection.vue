915d213c2de1984164276bb997a6fc17901a3d01
alialaca <alialaca0@gmail.com>
2025-04-09 14:47:55 +0300

    feat: update FooterSection and Logo components for improved styling and responsiveness

--- components/sections/FooterSection.vue
+++ components/sections/FooterSection.vue
@@ -13,26 +13,24 @@ const accounts = [
 const contacts = [
   { icon: 'ic:outline-location-on', text: 'Yedpa Ticaret Merkezi / İstanbul', link: '' },
   { icon: 'ic:outline-phone-in-talk', text: '+90 232 700 24 84', link: '' },
-  { icon: 'ic:sharp-language', text: 'www.fixpro.com.tr', link: 'https://www.fixpro.com.tr' },
+  { icon: 'ic:sharp-language', text: 'www.fixpro.com.tr', link: 'https://fixpro.com.tr' },
   { icon: 'ic:baseline-access-time', text: 'Pazartesi - Cuma / 09.00 - 17.00', link: '' }
 ]
 </script>
 
 <template lang="pug">
-section#iletisim(class="w-screen bg-fxsoftblue")
+section#iletisim(class="w-screen bg-fxblue text-white")
   div.container.mx-auto.pt-16.pb-8
     footer(class="flex items-start justify-between")
-      div.flex.flex-col.gap-3.text-fxblue
+      div.flex.flex-col.gap-3
         div.flex.items-end.gap-3
-          Logo
-          div(class="bg-fxblue h-[2rem] w-[1px]")
-          p.font-light Teknik Servis
-        div(class="bg-fxblue w-full h-[1px]")
-        div.flex.justify-between.text-fxblue
+          Logo(variation="white" )
+        div(class="bg-white w-full h-[1px]")
+        div.flex.justify-between
           LazyIcon(v-for="acount in accounts" :key="acount.link" :name="acount.icon")
         div.text-xs.mt-10 Copyright © 2025 | FixPro İç ve Dış Ticaret A.Ş
 
-      div.contacts.flex.flex-col.gap-2
+      div.contacts.flex.flex-col.gap-2.font-light
         a.flex.items-center(v-for="contact in contacts" :key="contact.text")
           Icon(:name="contact.icon" size="1em")
           p.ml-2 {{contact.text}}
--- components/shared/Logo.vue
+++ components/shared/Logo.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
-
+const {variation = 'normal'} = defineProps<{
+  variation: 'normal' | 'white'
+}>()
 </script>
 
 <template lang="pug">
-a(href="#" class="-m-1.5 p-1.5")
-  span(class="sr-only") FixPro
-  nuxt-img(src="/logo.png" class="h-8")
+  a(href="#" class="-m-1.5 p-1.5")
+    span(class="sr-only") FixPro
+    nuxt-img(:src="variation === 'white' ? '/logo-white.png' : '/logo.png'" class="h-8")
 </template>
 
 <style scoped>
