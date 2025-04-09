5425a347545288f8d575768c8f4b59edc8c0e06d
alialaca <alialaca0@gmail.com>
2025-04-09 14:32:48 +0300

    feat: enhance TutorialSection layout for mobile responsiveness and adjust carousel orientation

--- components/sections/TutorialSection.vue
+++ components/sections/TutorialSection.vue
@@ -2,6 +2,11 @@
 
 import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "~/components/ui/carousel";
 import TutorialCard from "~/components/TutorialCard.vue";
+import {breakpointsTailwind, useBreakpoints} from '@vueuse/core'
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+
+const isMobile = breakpoints.smallerOrEqual('md')
 
 const tutorials = [
   {
@@ -23,17 +28,17 @@ const tutorials = [
 </script>
 
 <template lang="pug">
-section#videolar.container.mx-auto.text-center.my-20.pb-10
-  p(class="text-4xl font-bold text-fxblue mt-20") Kullanım Videoları
-  p(class="text-muted-foreground mt-2 font-light") Temel bakım onarım işlemlerinizi cihazlarınızı bize göndermeden kendiniz de yapabilirsiniz
-  div(class="grid place-content-center pt-20")
-    Carousel(class="w-full max-w-screen-xl" :opts="{ align: 'start' }" )
-      CarouselContent(class="-ml-1" )
-        CarouselItem(v-for="(tutorial, index) in tutorials" :key="index" class="pl-1 md:basis-1/2 lg:basis-1/2")
-          div.p-1
-            tutorial-card(:title="tutorial.title" :description="tutorial.description" :videoUrl="tutorial.videoUrl")
-      CarouselPrevious
-      CarouselNext
+  section#videolar.container.mx-auto.text-center.my-20.pb-10(class="max-md:px-5")
+    p(class="text-4xl font-bold text-fxblue mt-20") Kullanım Videoları
+    p(class="text-muted-foreground mt-2 font-light") Temel bakım onarım işlemlerinizi cihazlarınızı bize göndermeden kendiniz de yapabilirsiniz
+    div(class="grid place-content-center pt-20")
+      Carousel(class="w-full max-w-screen-xl" :opts="{ align: 'start' }" :key="isMobile" :orientation="isMobile ? 'vertical' : 'horizontal'" )
+        CarouselContent(class="-ml-1" )
+          CarouselItem(v-for="(tutorial, index) in tutorials" :key="index + isMobile" class="pl-1 md:basis-1/2 lg:basis-1/2")
+            div.p-1
+              tutorial-card(:title="tutorial.title" :description="tutorial.description" :videoUrl="tutorial.videoUrl")
+        CarouselPrevious
+        CarouselNext
 </template>
 
 <style scoped>
