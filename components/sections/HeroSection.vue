cf4addba09fe1bc60df7de2e4bf38e635f46169a
alialaca <alialaca0@gmail.com>
2025-04-09 16:00:38 +0300

    feat: enhance HeroSection layout for improved mobile responsiveness and styling

--- components/sections/HeroSection.vue
+++ components/sections/HeroSection.vue
@@ -7,16 +7,16 @@ import {Input} from "~/components/ui/input";
 <template lang="pug">
   section#durum-sorgula(class="w-screen h-screen bg-fxsoftblue")
     app-navbar
-    div.container.mx-auto
+    div.container.mx-auto(class="max-md:mx-2")
       div
-        nuxt-img(src="/images/hero-image.png" class="w-[45vw] p-10 object-cover absolute right-10")
-        div(class="flex flex-col justify-center items-start absolute bottom-1/3")
-          h1(class="text-5xl font-bold text-fxblue") Servisteki cihazınızın
-          h1(class="text-5xl font-bold text-fxblue mt-2") durumunu sorgulayın
+        nuxt-img(src="/images/hero-image.png" class="w-[45vw] p-10 object-cover absolute right-10 max-md:w-full max-md:top-[30vw] max-md:translate-x-[10%]")
+        div(class="flex flex-col justify-center items-start absolute bottom-1/3 max-md:mx-2 max-md:top-[70vh]")
+          h1(class="text-5xl font-bold text-fxblue max-md:text-4xl") Servisteki cihazınızın
+          h1(class="text-5xl font-bold text-fxblue max-md:text-4xl mt-2") durumunu sorgulayın
           p(class="font-light mt-2") Lütfen servis numaranızı giriniz
 
-          div(class="flex w-full max-w-sm items-center gap-1.5 mt-20")
-            Input(id="servisno" type="text" maxlength="8" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '')" placeholder="Servis No" class="bg-white h-10 shadow-md font-bold uppercase text-fxblue text-xl placeholder:font-normal placeholder:text-sm placeholder:normal-case focus:border-fxblue" )
+          div(class="flex w-full max-w-md items-center gap-1.5 mt-20 max-md:mt-8")
+            Input(id="servisno" type="text" maxlength="8" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '')" placeholder="Servis No" class="bg-white h-10 shadow-md font-bold uppercase text-fxblue text-xl placeholder:font-normal placeholder:text-sm placeholder:normal-case focus:border-fxblue max-md:w-full" )
             Button(type="submit" class="bg-fxblue h-10 px-5 hover:bg-fxstatusblue hover:shadow-md" ) Sorgula
 
 </template>
