53cc3d66523ae11583c640c59a7c3111be4f4dcf
alialaca <alialaca0@gmail.com>
2025-04-09 16:24:03 +0300

    feat: update QuestionSection styling for improved minimum height on larger screens

--- components/sections/QuestionSection.vue
+++ components/sections/QuestionSection.vue
@@ -8,49 +8,48 @@ const contents = [
   {
     value: '1',
     question: "Onarım süreniz yaklaşık olarak kaç gündür?",
-    answer: ""
+    answer: "Cihazınızın onarım süresi servisimize vardıktan sonra normal şartlarda 5 iş günüdür. Ancak bu süre, cihazın arızasına ve yedek parça teminine bağlı olarak da değişiklik gösterebilir."
   },
   {
     value: '2',
     question: "Talep formu doldurdum cihazı size nasıl göndereceğim?",
-    answer: ""
+    answer: "Adres bilgileri ve kargo bilgileri talep formu doldurulduktan sonra size sms ile gönderilecektir. Bu bilgileri kullanarak cihazınızı kargo ile gönderebilirsiniz."
   },
   {
     value: '3',
     question: "Cihazı kılıfı ve veya bataryası olmadan size gönderebilir miyim?",
-    answer: ""
+    answer: "Evet gönderebilirsiniz. Elimizdeki bataryalar ile cihzınızı test edebiliriz. Ancak kılıfı olmadan gönderirseniz cihazınızın dış görünümünde hasar oluşabilir. Bu nedenle kılıfı ile göndermenizi öneririz."
   },
   {
     value: '4',
     question: "Vakit sorunu yaşıyorum cihazı size göndermeden kendim tamir edebilir miyim?",
-    answer: ""
+    answer: "Evet edebilirsiniz. Sitemizde bulunan kullanım videolarını izleyerek cihazınızı kendiniz tamir edebilirsiniz. Ancak bunun dışında bir arızaya sahipse cihazınızı kendiniz tamir etmeye çalışmayın. Bu durumda cihazınızın garantisi geçersiz olacaktır."
   },
   {
     value: '5',
     question: "Tamir sonrası gönderilen kargonun durumunu nasıl öğrenebilirim?",
-    answer: ""
+    answer: "Cihazınızn tamir süreci tamamlandıktan sonra size sms ile kargo takip numarası gönderilecektir. Bu takip numarasını kullanarak kargonuzun durumunu öğrenebilirsiniz."
   },
   {
     value: '6',
     question: "Web sitesinden detayları ne kadar süreyle görüntüleyebilirim?",
-    answer: ""
+    answer: "Web sitesindeki detayları 1 ay boyunca görüntüleyebilirsiniz. Dilerseniz kargonuz içerisinde size gönderilen bakım formunu veya tamir detay sayfasında bulunan bilgileri pdf olarak saklayabilirsiniz. Bu sayede detayları daha uzun süre saklayabilirsiniz."
   },
 ]
 </script>
 
 <template lang="pug">
-section#sikca-sorulan-sorular(class="w-screen h-screen bg-[url('/images/service.png')] bg-cover")
-  div.container.mx-auto.py-40(class="max-md:px-2")
-    Card(class="w-1/2 max-md:w-full ml-auto" )
-      CardHeader
-        CardTitle(class="text-2xl font-bold") Sıkça Sorulan Sorular
-      CardContent
-        Accordion(type="single" collapsible :default-value="defaultValue")
-          AccordionItem(v-for="content in contents" :key="content.value" :value="content.value")
-            AccordionTrigger {{ content.question }}
-            AccordionContent
-              p {{ content.answer }}
-              p anasayfasının altına sıkça sorulan sorular kısmı koymak istiyoruz ve kendimce örnek olarak koyabileceğimiz 4 soruyu aşağıda belirttim. Bu listeye eklemek/düzeltme/silmek istediğiniz soruları ekleyebilir misiniz? Web sitesine sizin daha önce ilettiğiniz ve yeni ileteceğiniz bakım videolarını da koyacağımızı göz önüne alarak da değerlendirebilirsiniz.
+  section#sikca-sorulan-sorular(class="w-screen min-h-screen bg-[url('/images/service.png')] bg-cover")
+    div.container.mx-auto.py-40(class="max-md:px-2")
+      Card(class="w-1/2 max-md:w-full ml-auto" )
+        CardHeader
+          CardTitle(class="text-2xl font-bold") Sıkça Sorulan Sorular
+        CardContent
+          Accordion(type="single" collapsible :default-value="defaultValue")
+            AccordionItem(v-for="content in contents" :key="content.value" :value="content.value")
+              AccordionTrigger {{ content.question }}
+              AccordionContent
+                p {{ content.answer }}
 
 </template>
 
