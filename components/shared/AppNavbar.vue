d53ee35884a38f3ed547c9da2fd52fec70caddf9
alialaca <alialaca0@gmail.com>
2025-04-09 16:00:48 +0300

    feat: update AppNavbar to include variation prop for Brand component

--- components/shared/AppNavbar.vue
+++ components/shared/AppNavbar.vue
@@ -30,7 +30,7 @@ const menues = [
         div(class="flex lg:flex-1")
           a(href="#" class="-m-1.5 p-1.5")
             span(class="sr-only") FixPro
-            Brand
+            Brand(variation="normal" )
         div(class="flex lg:hidden")
           button(type="button" class="-m-2.5 inline-flex items-center justify-center rounded-sm p-2.5 text-gray-700")
             span(class="sr-only") Open main menu
