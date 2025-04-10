1cae1a03253d08f6b31a4363d4bf986bb497ada8
alialaca <alialaca0@gmail.com>
2025-04-10 17:51:49 +0300

    refactor: refactor StatusTimelineItem.vue for improved template structure and readability

--- components/StatusTimelineItem.vue
+++ components/StatusTimelineItem.vue
@@ -11,21 +11,21 @@ defineProps<{
 </script>
 
 <template lang="pug">
-div.flex.flex-row.gap-4.pb-3
-  div(
-    class="border border-[3px] shadow-md border-fxstatusblue rounded-full grid place-content-center size-12 my-2"
-    :class="{'border-white': status.status !== 'completed', 'bg-[#EFEFF0]': status.status === 'pending'}"
+  div.flex.flex-row.gap-4.pb-3
+    div(
+      class="border border-[3px] shadow-md border-fxstatusblue rounded-full grid place-content-center size-12 my-2"
+      :class="{'border-white': status.status !== 'completed', 'bg-[#EFEFF0]': status.status === 'pending'}"
     )
-    Icon(:name="status.icon" class="text-fxblue size-6")
-  div(
-    v-show="status.title !== 'Teslim edildi'"
-    class="border-[.1rem] border-[#EFEFF0] h-8 relative -left-10 top-14"
-    :class="{'border-fxstatusblue': status.status === 'completed', 'border-fxstatusblue': status.status === 'completed'}"
+      Icon(:name="status.icon" class="text-fxblue size-6")
+    div(
+      v-show="status.title !== 'Teslim edildi'"
+      class="border-[.1rem] border-[#EFEFF0] h-8 relative -left-10 top-14"
+      :class="{'border-fxstatusblue': status.status === 'completed', 'border-fxstatusblue': status.status === 'completed'}"
     )
-  div.text-xs.my-2
-    p.text-sm {{ status.title }}
-    p.text-gray-500.font-light {{ status.status !== 'completed' ? 'Bekleniyor' : status.date }}
-    p.font-light {{ status.description ? status.description : '' }}
+    div.text-xs.my-2
+      p.text-sm {{ status.title }}
+      p.text-gray-500.font-light {{ status.status !== 'completed' ? 'Bekleniyor' : status.date }}
+      p.font-light {{ status.description ? status.description : '' }}
 </template>
 
 <style scoped>
