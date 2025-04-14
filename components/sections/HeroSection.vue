1d179f5f11a9febe06a54ad40c33ea6ea56f8a0f
alialaca <alialaca0@gmail.com>
2025-04-14 14:08:26 +0300

    fix: update HeroSection.vue to adjust positioning of text elements for better visibility on smaller screens

--- components/sections/HeroSection.vue
+++ components/sections/HeroSection.vue
@@ -10,7 +10,7 @@ import {Input} from "~/components/ui/input";
     div.container.mx-auto(class="max-md:mx-2")
       div
         nuxt-img(src="/images/hero-image.png" class="w-[50vw] p-10 object-cover absolute right-10 top-[50wh] max-lg:w-full max-md:top-[30vw] max-md:translate-x-[10%] max-lg:translate-x-[5%] max-md:translate-y-[-10%] max-lg:translate-y-[-10%] max-lg:top-[10vh]")
-        div(class="flex flex-col justify-center items-start absolute bottom-1/3 max-md:mx-2 max-md:top-[70vh] max-lg:top-[80vh]")
+        div(class="flex flex-col justify-center items-start absolute bottom-1/3 max-md:mx-2 max-md:bottom-[10vh] max-lg:bottom-[5vh]")
           h1(class="text-5xl font-bold text-fxblue max-md:text-4xl") Servisteki cihazınızın
           h1(class="text-5xl font-bold text-fxblue max-md:text-4xl mt-2") durumunu sorgulayın
           p(class="font-light mt-2") Lütfen servis numaranızı giriniz
