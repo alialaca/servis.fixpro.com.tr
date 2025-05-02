204e1a8aaf60ee757fed5bb1a98c583d2731d98e
alialaca <alialaca0@gmail.com>
2025-05-02 16:31:01 +0300

    feat: implement scheduled task to clean expired bans and configure nuxt-api-shield

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+    compatibilityDate: '2024-11-01',
+    devtools: {enabled: true},
 
-  modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@nuxt/eslint',
-    '@nuxt/icon',
-    '@nuxt/image',
-    '@nuxt/scripts'
-  ],
-  shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
-  }
+    modules: [
+        '@nuxtjs/tailwindcss',
+        'shadcn-nuxt',
+        '@nuxt/eslint',
+        '@nuxt/icon',
+        '@nuxt/image',
+        '@nuxt/scripts',
+        'nuxt-api-shield'
+    ],
+    shadcn: {
+        prefix: '',
+        componentDir: './components/ui'
+    },
+    nuxtApiShield: {
+        limit: {
+            max: 12,
+            duration: 108,
+            ban: 3600
+        }
+    },
+    nitro: {
+        storage: {
+            shield: {
+                driver: 'memory'
+            }
+        },
+        experimental: {
+            tasks: true
+        },
+        scheduledTasks: {
+            '*/15 * * * *': ['shield:clean']
+        }
+    }
 })
