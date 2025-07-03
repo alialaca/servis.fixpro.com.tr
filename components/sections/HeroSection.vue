49a9d7aa79a24b9a7d98a8ae156aabe4d93c4f2b
alialaca <alialaca0@gmail.com>
2025-07-03 16:31:06 +0300

    feat: integrate SearchForm into HeroSection and add loading state handling

--- components/sections/HeroSection.vue
+++ components/sections/HeroSection.vue
@@ -16,9 +16,7 @@ import SearchForm from "~/components/sections/SearchForm.vue";
           h1(class="text-5xl font-bold text-fxblue max-md:text-4xl mt-2") durumunu sorgulayın!
           p(class="font-light mt-2") Lütfen servis numaranızı giriniz
 
-          div(class="flex w-full max-w-md items-center gap-1.5 mt-20 max-md:mt-8")
-            Input(id="servisno" type="text" maxlength="8" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '')" placeholder="Servis No" class="bg-white h-10 shadow-md font-bold uppercase text-fxblue text-xl placeholder:font-normal placeholder:text-sm placeholder:normal-case focus:border-fxblue max-md:w-full" )
-            Button(type="submit" class="bg-fxblue h-10 px-5 hover:bg-fxstatusblue hover:shadow-md" ) Sorgula
+          SearchForm
 
 </template>
 
--- components/sections/SearchForm.vue
+++ components/sections/SearchForm.vue
@@ -1,14 +1,25 @@
 <script setup lang="ts">
+
 const kod = ref('')
 const loading = ref(false)
 const showError = ref(false)
+
+function check() {
+  loading.value = true
+  // if (kod.value) {
+  //   router.push(`/detay?kod=${kod.value}`)
+  // }
+  setTimeout(() => {
+    loading.value = false
+    showError.value = !showError.value
+  }, 3000)
+}
 
 </script>
 
 <template lang="pug">
 div(class="flex w-full max-w-md items-center gap-1.5 mt-20 max-md:mt-8")
   Input(id="servisno" v-model="kod" type="text" maxlength="8" oninput="this.value = this.value.replace(/[^a-zA-Z0-9]/g, '')" placeholder="Servis No" class="bg-white h-10 shadow-md font-bold uppercase text-fxblue text-xl placeholder:font-normal placeholder:text-sm placeholder:normal-case focus:border-fxblue max-md:w-full" )
-  Button(type="submit" @click="check" class="bg-fxblue h-10 px-5 hover:bg-fxstatusblue hover:shadow-md" ) Sorgula
   Button(type="submit" @click="check" class="bg-fxblue h-10 px-5 hover:bg-fxstatusblue hover:shadow-md" :disabled="loading" )
     | Sorgula
     Icon(v-if="loading" name="svg-spinners:180-ring" class="ml-2 text-white")
