9dfeef5daecbc737004f235509b6b197f4c964ed
alialaca <alialaca0@gmail.com>
2025-07-17 14:24:59 +0300

    feat: add dynamic servis detail handling with API integration, loading, and error states

--- pages/detay.vue
+++ pages/detay.vue
@@ -91,6 +91,47 @@ const items = [
     tutar: 150
   }
 ]
+
+const servisData = ref<any>(null)
+const loading = ref(false)
+const error = ref(false)
+
+onMounted(async () => {
+  const route = useRoute()
+  const kod = route.query.kod
+  
+  if (!kod) {
+    error.value = true
+    return
+  }
+  
+  const servisStore = useServisStore()
+  
+  // Önce store'dan kontrol et
+  const storeData = servisStore.getData(kod)
+  
+  if (storeData) {
+    // Store'dan kullan
+    servisData.value = storeData
+  } else {
+    // Store'da yok, API'den çek (sayfa yenileme durumu)
+    loading.value = true
+    try {
+      const data = await $fetch(`/api/detail/${kod}`, { method: 'GET' })
+      if (data) {
+        servisData.value = data
+        // Store'a da kaydet
+        servisStore.setData(kod, data)
+      } else {
+        error.value = true
+      }
+    } catch (err) {
+      error.value = true
+    } finally {
+      loading.value = false
+    }
+  }
+})
 </script>
 
 <template lang="pug">
@@ -98,68 +139,79 @@ main.w-screen.h-screen.bg-fwhite.flex
   div(class="w-4/12 max-lg:hidden h-screen bg-fxsoftblue grid place-content-center")
     img.px-10(src="/images/detail.png" alt="register")
   div(class="w-8/12 h-screen p-10 overflow-y-auto max-lg:w-full max-md:px-4 grid place-content-stretch max-md:place-content-start")
-    div(class="flex justify-between")
-      Brand(variation="normal" )
-      Button(variant="outline" class="text-fxblue")
-        Icon(name="ic:outline-local-printshop")
-        | Yazdır
-    div(class="w-full p-8 mt-4 border border-fxblue rounded-xl grid grid-cols-2")
-      div(class="max-lg:col-span-2")
-        p.text-2xl.font-bold Servis Kaydı #TS18675
-        p.text-xs.text-gray-500.font-light.mt-1 FixPro FX4566 | 12.05.2021
-        div.bilgiler(class="grid grid-cols-2 gap-4 gap-y-8 mt-4")
-          div.flex.flex-col.gap-1
-            p.text-sm.font-semibold.underline Müşteri Bilgileri
-            p.text-xs.font-light Ahmet Yılmaz
-            p.text-xs.font-light ahmet@email.com
-            p.text-xs.font-light +90 555 555 55 55
-          div.flex.flex-col.gap-1
-            p.text-sm.font-semibold.underline Cihaz Bilgileri
-            p.text-xs.font-light Model: FixPro FX165
-            p.text-xs.font-light Seri No: XM20235685899
-            p.text-xs.font-light Garanti: Garanti Kapsamında
-          div.flex.flex-col.gap-1.col-span-2
-            p.text-sm.font-semibold.underline Arıza açıklaması
-            p.text-xs.font-light Cihaz çalışırken aniden duruyor. Bekledikten sonra çalışıyor.
-          div.flex.flex-col.gap-1.col-span-2
-            p.text-sm.font-semibold.underline Cihaz Üzerinde Gelenler
-            p.text-xs.font-light Namlu, Batarya, Gaz, Çanta
-          div.flex.flex-col.gap-1.col-span-2
-            p.text-sm.font-semibold.underline Servis Değerlendirmesi
-            p.text-xs.font-light Batarya ısınma problemi çözüldü
-      div(class="border-l-[1px] border-dashed max-lg:border-t-[1px] max-lg:border-l-0 border-black pl-8 max-lg:pl-0 max-lg:pt-8 max-lg:mt-8 max-lg:col-span-2")
-        p.text-base.font-bold.underline.mb-4 Servis Durumu
-        status-timeline(:statuses="statuses")
-      Card.col-span-2.mt-5.shadow-none.border-black(class="max-lg:col-span-2")
-        CardHeader
-          CardTitle.text-lg Değiştirilen Parçalar
-        CardContent
-          Table
-            TableHeader
-              TableRow.font-bold
-                TableHead Ürün Kodu
-                TableHead(class="min-w-96" ) Ürün Adı
-                TableHead.text-right Fiyat (₺)
-                TableHead.text-right Miktar
-                TableHead.text-right Tutar (₺)
-            TableBody
-              TableRow(v-for="(item, index ) in items" :key="item.urunKodu + index" )
-                TableCell {{ item.urunKodu }}
-                TableCell(class="min-w-96" ) {{ item.urunAdi }}
-                TableCell.text-right {{ item.fiyat }}
-                TableCell.text-right {{ item.miktar }}
-                TableCell.text-right {{ item.tutar }}
-            TableFooter
-              TableRow.font-bold
-                TableCell TOPLAM
-                TableCell
-                TableCell.text-right
-                TableCell.text-right
-                TableCell.text-right 2900
-      div.w-full.mt-4.col-span-2
-        Button.bg-fxblue.float-right
-          Icon(name="ic:baseline-payments")
-          | Ödeme Yap
+    // Loading state
+    div(v-if="loading" class="flex justify-center items-center h-full")
+      Icon(name="svg-spinners:180-ring" class="text-fxblue text-6xl")
+    // Error state
+    div(v-else-if="error" class="flex flex-col justify-center items-center h-full")
+      Icon(name="heroicons:exclamation-triangle" class="text-red-500 text-6xl mb-4")
+      p.text-xl.font-bold.text-red-500 Servis kaydı bulunamadı
+      Button(@click="$router.push('/')" class="mt-4 bg-fxblue")
+        | Ana Sayfaya Dön
+    // Content
+    div(v-else-if="servisData")
+      div(class="flex justify-between")
+        Brand(variation="normal" )
+        Button(variant="outline" class="text-fxblue")
+          Icon(name="ic:outline-local-printshop")
+          | Yazdır
+      div(class="w-full p-8 mt-4 border border-fxblue rounded-xl grid grid-cols-2")
+        div(class="max-lg:col-span-2")
+          p.text-2xl.font-bold Servis Kaydı #TS18675
+          p.text-xs.text-gray-500.font-light.mt-1 FixPro FX4566 | 12.05.2021
+          div.bilgiler(class="grid grid-cols-2 gap-4 gap-y-8 mt-4")
+            div.flex.flex-col.gap-1
+              p.text-sm.font-semibold.underline Müşteri Bilgileri
+              p.text-xs.font-light Ahmet Yılmaz
+              p.text-xs.font-light ahmet@email.com
+              p.text-xs.font-light +90 555 555 55 55
+            div.flex.flex-col.gap-1
+              p.text-sm.font-semibold.underline Cihaz Bilgileri
+              p.text-xs.font-light Model: FixPro FX165
+              p.text-xs.font-light Seri No: XM20235685899
+              p.text-xs.font-light Garanti: Garanti Kapsamında
+            div.flex.flex-col.gap-1.col-span-2
+              p.text-sm.font-semibold.underline Arıza açıklaması
+              p.text-xs.font-light Cihaz çalışırken aniden duruyor. Bekledikten sonra çalışıyor.
+            div.flex.flex-col.gap-1.col-span-2
+              p.text-sm.font-semibold.underline Cihaz Üzerinde Gelenler
+              p.text-xs.font-light Namlu, Batarya, Gaz, Çanta
+            div.flex.flex-col.gap-1.col-span-2
+              p.text-sm.font-semibold.underline Servis Değerlendirmesi
+              p.text-xs.font-light Batarya ısınma problemi çözüldü
+        div(class="border-l-[1px] border-dashed max-lg:border-t-[1px] max-lg:border-l-0 border-black pl-8 max-lg:pl-0 max-lg:pt-8 max-lg:mt-8 max-lg:col-span-2")
+          p.text-base.font-bold.underline.mb-4 Servis Durumu
+          status-timeline(:statuses="statuses")
+        Card.col-span-2.mt-5.shadow-none.border-black(class="max-lg:col-span-2")
+          CardHeader
+            CardTitle.text-lg Değiştirilen Parçalar
+          CardContent
+            Table
+              TableHeader
+                TableRow.font-bold
+                  TableHead Ürün Kodu
+                  TableHead(class="min-w-96" ) Ürün Adı
+                  TableHead.text-right Fiyat (₺)
+                  TableHead.text-right Miktar
+                  TableHead.text-right Tutar (₺)
+              TableBody
+                TableRow(v-for="(item, index ) in items" :key="item.urunKodu + index" )
+                  TableCell {{ item.urunKodu }}
+                  TableCell(class="min-w-96" ) {{ item.urunAdi }}
+                  TableCell.text-right {{ item.fiyat }}
+                  TableCell.text-right {{ item.miktar }}
+                  TableCell.text-right {{ item.tutar }}
+              TableFooter
+                TableRow.font-bold
+                  TableCell TOPLAM
+                  TableCell
+                  TableCell.text-right
+                  TableCell.text-right
+                  TableCell.text-right 2900
+        div.w-full.mt-4.col-span-2
+          Button.bg-fxblue.float-right
+            Icon(name="ic:baseline-payments")
+            | Ödeme Yap
 
 
 </template>
